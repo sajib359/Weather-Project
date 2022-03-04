1ea8efd40f642a275bbe09ca6a5dce77256238b5
Sajib Hossain <sajib.sj359@gmail.com>
2022-03-04 12:08:44 +0600

    completed the project

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const API_KEY =`efa389eb8509af07e210f312451fab14`;
 
 const searchTemperature =()=>{
    const city = document.getElementById('search-input').value;
-   const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+   const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
    console.log(url);
 
@@ -13,6 +13,23 @@ const searchTemperature =()=>{
    .then(data=>displayTempture(data))
 }
 
-const displayTempture = temp =>{
-    console.log(temp);
+
+const innerTextWeather=(id , text)=>{
+    document.getElementById(id).innerText=text;
+}
+
+
+const displayTempture = temperature =>{
+    console.log(temperature);
+
+    innerTextWeather('city', temperature.name);
+    innerTextWeather('weather', temperature.main.temp);
+    innerTextWeather('weatherType', temperature.weather[0].main);
+
+    const url =` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+
+    const imageIcon = document.getElementById('weather-icon')
+    imageIcon.setAttribute('src',url);
+    
+
 }
